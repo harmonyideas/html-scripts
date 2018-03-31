0a397bd654d7b1cd228dbd73fdd44575a339fa59
HarmonyIdeas <8549684+harmonyideas@users.noreply.github.com>
2018-03-31 17:57:34 -0400

    Update congress-legislators.js

--- congress-legislators.js
+++ congress-legislators.js
@@ -5,7 +5,7 @@ var table = $("#details").DataTable();
 function loadJSON(callback) {
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'congress-legislators.json', true);
+    xobj.open('GET', 'data/congress-legislators.json', true);
     xobj.onreadystatechange = function() {
         if (xobj.readyState == 4 && xobj.status == "200") {
             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
