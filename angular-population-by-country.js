167668cdd98f554a80b57329c1e7615cae64155e
HarmonyIdeas <8549684+harmonyideas@users.noreply.github.com>
2018-04-14 01:11:53 -0400

    Add pagination

--- angular-population-by-country.js
+++ angular-population-by-country.js
@@ -1,7 +1,11 @@
-var App = angular.module('App', []);
+var App = angular.module('App', ['ui.bootstrap']);
 
 App.controller('MyCtrl', function($scope, $http) {
 
+    $scope.filteredList = [],
+        $scope.currentPage = 1,
+        $scope.numPerPage = 10,
+        $scope.maxSize = 5;
 
     $scope.sortType = 'population'; // set the default sort type
     $scope.sortReverse = true; // set the default sort order
@@ -10,8 +14,14 @@ App.controller('MyCtrl', function($scope, $http) {
     $http.get('data/worldpopulation.json')
         .then(function(res) {
             $scope.data = res.data;
+            $scope.totalItems = $scope.data.length;
             angular.forEach($scope.data, function(country) {
                 country.population = parseInt(country.population);
+            });
+            $scope.$watch("currentPage + numPerPage", function() {
+                var begin = (($scope.currentPage - 1) * $scope.numPerPage),
+                    end = begin + $scope.numPerPage;
+                $scope.filteredList = $scope.data.slice(begin, end);
             });
         });
 });
