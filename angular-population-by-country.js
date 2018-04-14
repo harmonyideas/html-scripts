50f76cb2fa5777d726495f2650e89acee9dc24cc
HarmonyIdeas <8549684+harmonyideas@users.noreply.github.com>
2018-04-14 02:27:46 -0400

    Update angular-population-by-country.js

--- angular-population-by-country.js
+++ angular-population-by-country.js
@@ -2,26 +2,26 @@ var App = angular.module('App', ['ui.bootstrap']);
 
 App.controller('MyCtrl', function($scope, $http) {
 
-    $scope.filteredList = [],
-        $scope.currentPage = 1,
-        $scope.numPerPage = 10,
-        $scope.maxSize = 5;
+  $scope.filteredList = [],
+    $scope.currentPage = 1,
+    $scope.numPerPage = 10,
+    $scope.maxSize = 5;
 
-    $scope.sortType = 'population'; // set the default sort type
-    $scope.sortReverse = true; // set the default sort order
-    $scope.searchCountry = ''; // set the default search/filter term
+  $scope.sortType = 'population'; // set the default sort type
+  $scope.sortReverse = true; // set the default sort order
+  $scope.searchCountry = ''; // set the default search/filter term
 
-    $http.get('data/worldpopulation.json')
-        .then(function(res) {
-            $scope.data = res.data;
-            $scope.totalItems = $scope.data.length;
-            angular.forEach($scope.data, function(country) {
-                country.population = parseInt(country.population);
-            });
-            $scope.$watch("currentPage + numPerPage", function() {
-                var begin = (($scope.currentPage - 1) * $scope.numPerPage),
-                    end = begin + $scope.numPerPage;
-                $scope.filteredList = $scope.data.slice(begin, end);
-            });
-        });
+  $http.get('data/worldpopulation.json')
+    .then(function(res) {
+      $scope.data = res.data;
+      $scope.totalItems = $scope.data.length;
+      angular.forEach($scope.data, function(country) {
+        country.population = parseInt(country.population);
+      });
+      $scope.$watch("currentPage + numPerPage", function() {
+        var begin = (($scope.currentPage - 1) * $scope.numPerPage),
+          end = begin + $scope.numPerPage;
+        $scope.filteredList = $scope.data.slice(begin, end);
+      });
+    });
 });
