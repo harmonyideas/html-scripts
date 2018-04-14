fd6a53245a7f9952f168ac2fbb1e8463b590fe46
HarmonyIdeas <8549684+harmonyideas@users.noreply.github.com>
2018-04-14 02:32:33 -0400

    Cleanup

--- angular-population-by-country.js
+++ angular-population-by-country.js
@@ -2,14 +2,16 @@ var App = angular.module('App', ['ui.bootstrap']);
 
 App.controller('MyCtrl', function($scope, $http) {
 
+//Pagination Settings
   $scope.filteredList = [],
   $scope.currentPage = 1,
   $scope.numPerPage = 10,
   $scope.maxSize = 5;
+//Default Settings
   $scope.sortType = 'population'; // set the default sort type
   $scope.sortReverse = true; // set the default sort order
   $scope.searchCountry = ''; // set the default search/filter term
-
+//Read JSON data and convert population to Int (quotes are used for JSON data)
   $http.get('data/worldpopulation.json')
     .then(function(res) {
       $scope.data = res.data;
