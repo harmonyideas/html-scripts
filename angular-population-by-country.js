00683e4f377c7090011a82b10f7de60bf2722e2c
HarmonyIdeas <8549684+harmonyideas@users.noreply.github.com>
2018-04-18 12:35:57 -0400

    Fix pagination search issue

--- angular-population-by-country.js
+++ angular-population-by-country.js
@@ -1,28 +1,33 @@
-var App = angular.module('App', ['ui.bootstrap']);
+var App = angular.module('myApp', ['ui.bootstrap']);
 
-App.controller('MyCtrl', function($scope, $http) {
-
-//Pagination Settings
-  $scope.filteredList = [],
-  $scope.currentPage = 1,
-  $scope.numPerPage = 10,
-  $scope.maxSize = 5;
-//Default Settings
-  $scope.sortType = 'population'; // set the default sort type
-  $scope.sortReverse = true; // set the default sort order
-  $scope.searchCountry = ''; // set the default search/filter term
-//Read JSON data and convert population to Int (quotes are used for JSON data)
-  $http.get('data/worldpopulation.json')
-    .then(function(res) {
-      $scope.data = res.data;
-      $scope.totalItems = $scope.data.length;
-      angular.forEach($scope.data, function(country) {
-        country.population = parseInt(country.population);
-      });
-      $scope.$watch("currentPage + numPerPage", function() {
-        var begin = (($scope.currentPage - 1) * $scope.numPerPage),
-          end = begin + $scope.numPerPage;
-        $scope.filteredList = $scope.data.slice(begin, end);
-      });
-    });
+App.filter('startFrom', function() {
+  return function(input, start) {
+    if (input) {
+      start = +start;
+      return input.slice(start);
+    }
+    return [];
+  }
+});
+App.controller('controller', function($scope, $http, $timeout) {
+  $http.get('data/worldpopulation.json').then(function(response) {
+    // Default Settings
+    $scope.data = response.data;
+    $scope.currentPage = 1;
+    $scope.numPerPage = 10;
+    $scope.filter_data = $scope.data.length;
+    $scope.totalItems = $scope.data.length;
+  });
+  $scope.page_position = function(page_number) {
+    $scope.currentPageat = page_number;
+  };
+  $scope.filter = function() {
+    $timeout(function() {
+      $scope.filter_data = $scope.searched.length;
+    }, 20);
+  };
+  $scope.sort_with = function(base) {
+    $scope.base = base;
+    $scope.reverse = !$scope.reverse;
+  };
 });
