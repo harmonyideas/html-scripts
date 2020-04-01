ff71d65260ee2c30b71de19210e3828dd05a606c
HarmonyIdeas <8549684+harmonyideas@users.noreply.github.com>
2020-03-31 20:52:04 -0400

    Create import-csvdata.py

---
+++ leaflet-map/import-csvdata.py
@@ -0,0 +1,33 @@
+import csv
+import urllib2
+import pandas as pd
+import json
+import sys
+import datetime 
+from datetime import timedelta
+import pytz
+
+file_date = datetime.date.strftime(datetime.datetime.now(pytz.timezone('US/Eastern')), "%m-%d-%Y")
+url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/%s' % file_date
+try:
+    response = urllib2.urlopen(url)
+except urllib2.URLError as err:
+    sys.exit(1)
+
+covid19_data = pd.read_csv(response, iterator=True, chunksize=1000)
+covid19_df = pd.concat([chunk[chunk['Province_State'] == 'New Jersey'] for chunk in covid19_data])
+file_local = '/usr/local/bin/www-scripts/njcounties.json'
+file_www = '/var/www/html/public/map/njcounties.json'
+with open(file_local, 'r') as f:
+    data = json.load(f)
+
+for i, updaterow in covid19_df.iterrows():
+    #print updaterow['FIPS']
+    for row in data['features']:
+        if (row['properties']['NAME'] == updaterow['Admin2']):
+            row['properties']['COVID19_CASES'] = updaterow['Confirmed']
+            row['properties']['COVID19_DEATHS'] = updaterow['Deaths']
+        
+
+with open(file_www, 'w+') as f:
+    json.dump(data, f, indent=2)
