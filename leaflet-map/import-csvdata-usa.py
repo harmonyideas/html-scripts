886a259a322a8a67074079086b1722d7d56d11a3
HarmonyIdeas <8549684+harmonyideas@users.noreply.github.com>
2021-11-12 09:42:48 -0500

    Update import-csvdata-usa.py

--- leaflet-map/import-csvdata-usa.py
+++ leaflet-map/import-csvdata-usa.py
@@ -1,1 +1,37 @@
+import csv
+import urllib2
+import pandas as pd
+import json
+import sys
+import datetime
+from datetime import timedelta
+import pytz
+
+file_date = datetime.datetime.now(pytz.timezone('US/Eastern'))
+file_date -= datetime.timedelta(days=1)
+file_date = file_date.strftime("%m-%d-%Y") + '.csv'
+url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/%s' % file_date
+
+try:
+    response = urllib2.urlopen(url)
+except urllib2.URLError as err:
+    print(err)
+    sys.exit(1)
+
+covid19_data = pd.read_csv(response, iterator=True, chunksize=1000)
+covid19_df = pd.concat([chunk for chunk in covid19_data])
+file_local = '/usr/local/bin/www-scripts/usacounties.json'
+file_www = '/var/www/html/harmonyideas.com/map/usacounties.json'
+with open(file_local, 'r') as f:
+    data = json.load(f)
+
+for updaterow in covid19_df.itertuples():
+    for row in data['features']:
+        if (float(row['properties']['GEOID']) == updaterow.FIPS):
+                row['properties']['COVID19_CASES'] = updaterow.Confirmed
+                row['properties']['COVID19_DEATHS'] = updaterow.Deaths
+
+
+with open(file_www, 'w+') as f:
+    json.dump(data, f, separators=(',', ':'))
 
