7850843ee22ec8c1119c524e0f29a3cf07cf3840
HarmonyIdeas <8549684+harmonyideas@users.noreply.github.com>
2023-10-22 18:53:41 -0400

    Update jquery-congress-legislators.js
    Cleanup code

--- jquery-congress-legislators.js
+++ jquery-congress-legislators.js
@@ -1,100 +1,94 @@
-// Global JSON array
-var dataArray = [];
-var table = $("#details").DataTable(
-    {
-        "columnDefs": [
-            {
-                "targets": -1,
-                "defaultContent": "<i>Unavailable</i>"
-            }
-        ],
-        "columns": [
-            null,
-            null,
-            null,
-            null,
-            null,
-            {
-                "defaultContent": '<i>Unavailable</i>'
-            },
-            {
-                fnCreatedCell: function (nTd, sData, oData, iRow, iCol) {
-                    if (oData[5]) {
-                        $(nTd).html("<a href='https://www.c-span.org/person/?" + oData[5] + "'>CSPAN link</a>");
-                        console.log(oData[5]);
-                    }
-
-                }
-            },
-        ]
-    }
-);
-
-function loadJSON(callback) {
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'data/congress-legislators.json', true);
-    xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-        }
-    };
-    xobj.send(null);
-}
-loadJSON(function (response) {
-    dataArray = JSON.parse(response);
-    var statesArray = [];
-    var filterArray = [];
-
-    // Create array to hold both abbreviated and full state names
-    $.each(dataArray[0].states, function (i, item) {
-        var states = [
-            [i],
-            [item]
-        ];
-        statesArray.push(states);
-    });
-
-    // Sorting the states 
-    statesArray.sort();
-    var $stateDropDown = $("#DropDown_State");
-
-    // Build the list of US states for user to select
-    $.each(statesArray, function (i, item) {
-        $stateDropDown.append('<option value="' + item[0] + '">' + item[1] + '</option>');
-    });
-
-    // Return senator/representative term information based on state selected
-    $stateDropDown.change(function () {
-        var selectedstate = this.value;
-        //filter based on selected state.
-        filterArray = jQuery.grep(dataArray, function (item, i) {
-            return item.terms[0].state == selectedstate;
-        });
-        updateTable(filterArray);
-    });
-})
-// Update the datatable element with array data
-updateTable = function (collection) {
-    table.clear();
-    for (var i = 0; i < collection.length; i++) {
-        table.row.add([collection[i].name.official_full,
-        collection[i].terms[collection[i].terms.length - 1].start,
-        collection[i].terms[collection[i].terms.length - 1].end,
-        collection[i].terms[collection[i].terms.length - 1].type,
-        collection[i].terms[collection[i].terms.length - 1].party,
-        collection[i].id.cspan
-        ]).draw(true);
-    }
-}
-
-$('#details').delegate("tr.rows", "click", function () {
-    alert("Click!");
+// Declare variables
+const dataArray = [];
+const table = $("#details").DataTable({
+	columnDefs: [{
+		targets: -1,
+		defaultContent: "<i>Unavailable</i>"
+	}],
+	columns: [
+		null,
+		null,
+		null,
+		null,
+		null,
+		{
+			defaultContent: "<i>Unavailable</i>"
+		},
+		{
+			fnCreatedCell: function(nTd, sData, oData, iRow, iCol) {
+				if (oData[5]) {
+					$(nTd).html(
+						`<a href='https://www.c-span.org/person/?${oData[5]}'>CSPAN link</a>`
+					);
+				}
+			}
+		}
+	]
 });
 
-$('#details').on('click', 'td', function (e) {
-    var table = $('#details').DataTable();
-    let data = table.row(e.target.closest('tr')).data();
-})
+// Load JSON data
+async function loadJSON() {
+	const response = await fetch("data/congress-legislators.json");
+	const data = await response.json();
+	dataArray.push(...data);
+}
 
+// Create a list of states
+async function createStateList() {
+	const statesArray = [];
+	const states = dataArray[0].states;
+	for (const [abbreviation, fullName] of Object.entries(states)) {
+		statesArray.push([abbreviation, fullName]);
+	}
+	statesArray.sort();
+	return statesArray;
+}
+
+// Update the datatable with a filtered list of legislators
+async function updateTable(filterArray) {
+	table.clear();
+	for (const legislator of filterArray) {
+		table.row.add([
+			legislator.name.official_full,
+			legislator.terms[legislator.terms.length - 1].start,
+			legislator.terms[legislator.terms.length - 1].end,
+			legislator.terms[legislator.terms.length - 1].type,
+			legislator.terms[legislator.terms.length - 1].party,
+			legislator.id.cspan
+		]).draw(true);
+	}
+}
+
+// Initialize the page
+async function init() {
+	await loadJSON();
+	const statesArray = await createStateList();
+
+	// Build the state dropdown
+	const $stateDropDown = $("#DropDown_State");
+	for (const state of statesArray) {
+		const [abbreviation, fullName] = state;
+		$stateDropDown.append(`<option value="${abbreviation}">${fullName}</option>`);
+	}
+
+	// Filter the legislators on state change
+	$stateDropDown.change(async function() {
+		const selectedState = this.value;
+		const filterArray = dataArray.filter(
+			(legislator) => legislator.terms[0].state === selectedState
+		);
+		await updateTable(filterArray);
+	});
+
+	// Add click event listeners to the datatable
+	$("#details").delegate("tr.rows", "click", function() {
+		alert("Click!");
+	});
+
+	$("#details").on("click", "td", function(e) {
+		const table = $("#details").DataTable();
+		const data = table.row(e.target.closest("tr")).data();
+	});
+}
+
+init();
