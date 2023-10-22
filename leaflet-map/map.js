6af1f60604ac87dd5d8cbfb9e430ab4a9e69a3a8
HarmonyIdeas <8549684+harmonyideas@users.noreply.github.com>
2023-10-22 13:58:02 -0400

    Update map.js
    Cleanup

--- leaflet-map/map.js
+++ leaflet-map/map.js
@@ -23,7 +23,7 @@ map.on('drag', function() {
 });
 
 // load a tile layer
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=', {
     minZoom: 7,
     maxZoom: 9,
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
